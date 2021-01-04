b279bd858160c4dce3dfad4a67027e67eb480190
oscarludick <oscarludick@gmail.com>
2021-01-04 11:57:38 -0600

    update: refactor

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { Notifications } from "./notifications-observer/notifications";
-import { SMSNotification } from "./notifications-observer/sms-notifications";
-import { EmailNotification } from "./notifications-observer/email-notifications";
+import { Notifications } from "./observables/notifications";
+import { SMSNotification } from "./observers/sms-notifications";
+import { EmailNotification } from "./observers/email-notifications";
 
 const notifications = new Notifications();
 
